6105217fc78e52f0a3be4a404323b94af114d17a
SantiagoGaona <sgaonacarvajal@outlook.com>
2023-07-13 22:32:26 -0500

    feat(frontend): add new Metadata and favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { AuthProvider } from "@/context/AuthProvier";
 import "./globals.css";
+import { Metadata } from "next";
 import theme from "@/theme/theme";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 import { TaskProvider } from "@/context/TaskContext";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Epic Organizer",
   description: "Organize your life by tasks",
 };
