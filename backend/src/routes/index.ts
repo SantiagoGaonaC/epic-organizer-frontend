570ecbdf50bf8f69faacf23abefcfa24dc3db0d5
SantiagoGaona <sgaonacarvajal@outlook.com>
2023-07-16 16:55:19 -0500

    refactor: changing folder structures

--- backend/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authRouter from "./auth";
-import calendarRouter from "./calendar";
-const router = express.Router();
-
-router.use("/auth", authRouter);
-router.use("/calendar", calendarRouter);
-
-export default router;
